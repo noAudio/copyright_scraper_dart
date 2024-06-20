52f87636b931112b7333bd50159db2d5347cbdd9
Samuel Odongo <samieodd@gmail.com>
2024-06-21 01:54:43 +0300

    Create data class for copyright info

---
+++ lib/copyright_info.dart
@@ -0,0 +1,15 @@
+class CopyRightInfo {
+  String registrationNumberAndDate;
+  String applicationTitle;
+  String copyrightClaimant;
+  String rightsAndPermissions;
+  String typeOfWork;
+
+  CopyRightInfo({
+    required this.registrationNumberAndDate,
+    required this.applicationTitle,
+    required this.copyrightClaimant,
+    required this.rightsAndPermissions,
+    required this.typeOfWork,
+  });
+}
