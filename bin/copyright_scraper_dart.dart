4f93c21341348a8810b5c4cbb2c02e25a97e0184
Samuel Odongo <samieodd@gmail.com>
2024-06-21 14:03:33 +0300

    Complete scraping logic

--- bin/copyright_scraper_dart.dart
+++ bin/copyright_scraper_dart.dart
@@ -1,6 +1,33 @@
+import 'dart:io';
+
+import 'package:copyright_scraper_dart/copyright_info.dart';
 import 'package:copyright_scraper_dart/copyright_scraper_dart.dart';
 
 void main(List<String> arguments) async {
-  Scraper scraper = Scraper(year: '2024', pages: 222);
-  await scraper.getData();
+  String year = '2023';
+  int pages = 287;
+  String startDate = '2023-01-01';
+  String endDate = '2023-06-30';
+  // var dateRange = year == '2024'
+  //     ? ['2024-01-01', '2024-06-20']
+  //     : ['$year-01-01', '$year-12-31'];
+  var total = <CopyRightInfo>[];
+  for (int page = 1; page <= pages; page++) {
+    Scraper scraper = Scraper(year: year, pages: pages);
+    // if (page == 5) break;
+    print('$page of $pages');
+    await scraper.getData(
+        'https://publicrecords.copyright.gov/advanced-search?page_number=$page&parent_query=%5B%7B%22operator_type%22:%22AND%22,%22column_name%22:%22all_copyright_numbers%22,%22type_of_query%22:%22starts_with%22,%22query%22:%22TXu%22%7D%5D&records_per_page=100&sort_order=%22asc%22&highlight=true&model=%22%22&date_field=%22registration_date_as_date%22&start_date=%22$startDate%2000:00:00%22&end_date=%22$endDate%2000:00:00%22');
+    for (var result in scraper.results) {
+      total.add(result);
+    }
+    // print(total.length);
+  }
+  File file = File('$year-p1-records.csv');
+  for (var result in total) {
+    file.writeAsStringSync(
+      '"${result.registrationNumberAndDate}", "${result.applicationTitle}", "${result.copyrightClaimant}", "${result.rightsAndPermissions}", "${result.typeOfWork}"\n',
+      mode: FileMode.append,
+    );
+  }
 }
--- lib/copyright_scraper_dart.dart
+++ lib/copyright_scraper_dart.dart
@@ -1,3 +1,7 @@
+import 'dart:async';
+import 'dart:io';
+import 'dart:math';
+
 import 'package:copyright_scraper_dart/copyright_info.dart';
 import 'package:puppeteer/puppeteer.dart';
 
@@ -7,6 +11,7 @@ class Scraper {
   late List<String> dateRange;
   late Browser _browser;
   late Page _page;
+  late Completer<void> _completer;
   String originLink = 'https://publicrecords.copyright.gov/';
   List<CopyRightInfo> results = [];
 
@@ -15,6 +20,7 @@ class Scraper {
     required this.pages,
   }) {
     dateRange = setDateRange(year = year);
+    _completer = Completer<void>();
   }
 
   List<String> setDateRange(String year) {
@@ -25,12 +31,65 @@ class Scraper {
   }
 
   Future<void> setUpBrowser() async {
-    _browser = await puppeteer.launch(headless: false);
+    _browser = await puppeteer.launch(headless: true);
     _page = await _browser.newPage();
-    await _page.goto(originLink, wait: Until.domContentLoaded);
+    // await _page.goto(originLink, wait: Until.domContentLoaded);
   }
 
-  Future<void> getData() async {
+  Future<void> getData(String link) async {
     await setUpBrowser();
+
+    await _page.goto(link, wait: Until.domContentLoaded);
+    sleep(Duration(seconds: Random().nextInt(6) + 3));
+    var url = '';
+    _page.onResponse.listen((response) async {
+      url = response.url;
+      if (url ==
+          'https://api.publicrecords.copyright.gov/search_service_external/advance_search') {
+        if (response.status == 200) {
+          try {
+            var payload = await response.json;
+            extractInfo(payload);
+          } catch (e) {
+            print(e);
+          }
+        }
+      }
+    });
+    // print(results[results.length - 1].applicationTitle);
+    await _completer.future;
+    await _browser.close();
+  }
+
+  void extractInfo(dynamic payload) {
+    List<dynamic> data = payload['data'];
+    int length = results.length;
+    for (var item in data) {
+      String regNoAndDate =
+          "${item['hit']['registration_number']} / ${item['hit']['representative_date']}";
+      String applicationTitle = item['hit']['title_application_title'][0];
+      String copyrightClaimant = item['hit']['claimants_list'] != null
+          ? "${item['hit']['claimants_list'][0]['claimant_full_name']}, ${item['hit']['claimants_list'][0]['claimant_dates'] != null ? item['hit']['claimants_list'][0]['claimant_dates'].split(' ')[0] : ''}. ${item['hit']['claimants_list'][0]['claimant_address']}"
+          : 'n/a';
+      String rightsPermissions =
+          item['hit']['rights_and_permissions_statement'] != null
+              ? item['hit']['rights_and_permissions_statement'].join(' ')
+              : 'n/a';
+      String typeWork = item['hit']['type_of_work'];
+
+      results.add(
+        CopyRightInfo(
+          registrationNumberAndDate: regNoAndDate,
+          applicationTitle: applicationTitle,
+          copyrightClaimant: copyrightClaimant,
+          rightsAndPermissions: rightsPermissions,
+          typeOfWork: typeWork,
+        ),
+      );
+    }
+    if (results.length == length + data.length) {
+      _completer.complete();
+    }
+    // print(results.length);
   }
 }
